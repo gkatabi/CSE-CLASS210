66902f4da16494df26114a3c2cabc4ef311e147e
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-28 23:45:16 +0200

    Update Program.cs
    "added options"

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,5 +8,12 @@ class Program
         DateTime now = DateTime.Now;
         Console.WriteLine(now);
         Console.ReadLine();
+        Console.WriteLine("This reminder for: 1)waking up \n2)Important event \nI have finished n\It's records");        
+        Console.Write("Chose a number for  this time's reminder: ")
+        int numberChozen = int.perse(Console.ReadLine());
+
+        
+
+      
     }
 }
