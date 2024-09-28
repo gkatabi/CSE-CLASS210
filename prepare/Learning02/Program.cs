a1f92d97a9b3bf8fdcc7e6b9e45344cae9a6b3e4
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-28 15:44:15 +0200

    Update Program.cs
    "ok"

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,19 +1,9 @@
-/*using System;
-public class jobTittle
+using System;
+
+class Program
 {
-    public string_jobName = " "
-    public string_jobResponsibility = " "
-    public int_startYear = "";
-    public int_endYear = "";
-
-        public jobTittle( )
-        {
-        }
-        public void ShowJobTittle()
-        {
-            Console.WriteLine($"{_jobTittle}";
-            Console.WriteLine($"as a{"_jobResponsibility}";
-            Console. WriteLine($"{_startYear},'',{_endYear}";
-
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Learning03 World!");
     }
-}*/
+}
