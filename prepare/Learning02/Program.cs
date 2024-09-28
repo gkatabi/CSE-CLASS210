e12f2fa6c6acb9366449a037def033ab363ad638
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-28 15:49:29 +0200

    Update Program.cs
    "cont"

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Console.Write("Enter First Name:");
+        string firstName =           Console.ReadLine();
+        Console.Write("Enter Last Name:");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"The name is : {firstName} {lastName}");
+
     }
 }
