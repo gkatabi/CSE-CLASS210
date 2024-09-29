142d1e511738f65bf32ba2275b21ed4d69f5a234
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-29 16:36:12 +0200

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,7 +8,7 @@ class Program
         DateTime now = DateTime.Now;
         Console.WriteLine(now);
         Console.ReadLine();
-        Console.WriteLine("This reminder for: \r\nI1)waking up \r\nI2)Important event \nI3) have finished \r\nIIt's records");        
+        Console.WriteLine("This reminder for: \r\nI1.waking up \r\nI2.Important event \nI3.I have finished \r\nI4.It's recorded \r\nI5.Q");        
         Console.Write("Chose a number for  this time's reminder: ")
         int numberChozen = int.perse(Console.ReadLine());
 
