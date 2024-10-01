d4bc8511507c83bac67a7f26c7daf9eb036d2318
katabira <gkatemye@gmail.com>
2024-10-01 05:58:44 +0200

    "changed the welcoming remarks"

---
+++ Class project.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* >= 90...A
+              >= 80...B
+              >= 70...C
+              >= 60...D
+              <  50...F*/
+            Console.WriteLine("This is the grade finder");
+            Console.WriteLine("What was your score?: ");
+            string score = Console.ReadLine();
+            int Score = Convert.ToInt32(Console.ReadLine());
+            if (Score >= 90)
+            {
+                Console.WriteLine("You passed with: A");
+            else if (Score >= 80)
+                Console.WriteLine("You have passed: B");
+            else if (Score >= 70)
+                Console.WriteLine("You passed with:C");
+            else if (Score < 60)
+                Console.WriteLine("You passed with: D");
+            }
+            else
+            {
+                Console.WriteLine("You failed with: F");
+
+            }
+        }
+    }
+}
+ 
+Sent from Mail for Windows
+ 
