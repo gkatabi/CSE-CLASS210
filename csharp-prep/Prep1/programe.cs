85c9807366465a6e9dd7912883ce535d4473b821
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-10 08:59:37 +0200

    Update programe.cs
    changed the whole code

--- csharp-prep/Prep1/programe.cs
+++ csharp-prep/Prep1/programe.cs
@@ -4,11 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?: ");
-        string firstName = Console.ReadLine();
-        Console.WriteLine("What is your last name?: ");
-        string last name = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is: " {firstName} , {lastName}); 
+        Console.WriteLine("Hello Prep1 World!");
+    {
+        Try to restructure the logic so it does exactly what you want.
+
+Random r = new Random();
+
+int val = r.Next(1, 100);
+int guess = 0;
+bool correct = false;
+
+Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+while (!correct)
+{
+    Console.Write("Guess: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out guess))
+    {
+        Console.WriteLine("That's not a number.");
+        continue;
+    }
+
+    if (guess < val)
+    {
+        Console.WriteLine("No, the number I'm thinking is higher than that number.");
+    }
+    else if (guess > val)
+    {
+        Console.WriteLine("No, the number I'm thinking is lower than that number.");
+    }
+    else
+    {
+        correct = true;
+        Console.WriteLine("You guessed right!");
+    }
     }
 }
