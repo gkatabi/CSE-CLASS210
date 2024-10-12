ca7fa3ab24e8d3795a0ca1292790c151ff8442ee
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-12 11:54:45 +0200

    Update Program.cs
    now

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -3,39 +3,42 @@ using System;
 class labels
 //I will use the getters setters 
 {
-    class packing label
+    public class packing
     {
         private string name;
         public string Name
         {
-            get {return name;}
-            set {name = value;}
+            get { return name; }
+            set { name = value; }
         }
-    class shippingLabel
-    {
-        private string destiny;
-        public string Destiny
+        class shippingLabel
         {
-            get{return destiny;}
-            set{destiny= value;}
+            public string destiny;
+            public string Destiny
+            {
+                get { return destiny; }
+                set { destiny = value; }
+            }
         }
-    }
-    class program
-    { 
-        static void main(string[]args)
-        { 
-            //Detail company label
-            packingLabel myobj = new packingLabel();
-            myobj.name = "Alma packages"
-            
-            //Detail destiny
-            shippingLabel myobj = new shippingLabel();
-            myobj.destiny = "42 Orlando Crescent USA"
-            Console.WriteLine(myobj.Name);
-            Console.WriteLine(myobj.Destiny);
-        }
-        
-    }
+        class program
+        {
+            static void main(string[] args)
+            {
+                //Detail company label
+                shippingLabel myobj = new shippingLabel();
+                myobj.destiny = "Alma packages";
+                myobj.display();
 
-}    
-        
+                //Detail destiny
+                shippingLabel.myobj = new shippingLabel();
+                myobj.destiny = "42 Orlando Crescent USA";
+                Console.WriteLine(myobj.destiny);
+                Console.WriteLine(myobj.Destiny);
+            }
+
+        }
+
+    }
+}
+
+ 
