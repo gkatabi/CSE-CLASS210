8fd9b7ad8eb46eb4995793c5724ba40e2ce6a285
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-14 00:09:10 +0200

    Update Program.cs
    removed the day

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,1 +1,183 @@
-da
+// static members of the outer class
+using System;
+namespace MyApplication
+{
+// Outer class
+    public class Video_class
+
+    {
+ 
+    // Static data member of the video class
+        public  string tittle = "Red Rat";
+        public string author ="Johnson moody";
+        public int length = "366";
+         
+    // Inner class
+        public class Comment_class 
+        {
+ 
+        // Static method of Inner class
+            public static void method1()
+            {
+           
+            // Displaying the value of a
+            // static member of the Video class
+            string identity = Console.writeLine("Provide your names, address  & coment");
+            string identity = Console.writeLine("Provide your names, address  & coment");
+            string identity = Console.Write("Your personal name ");
+            string identity = Console.ReadLine();
+            string house = Console.Write("House number: ")
+            int house = int.parse(Console.ReadLine());
+            string street = Console.Write( "Name of your street: ") ;
+            address = Console.ReadLine();
+            string city = Console.Write("City: ");
+            string city = Console.ReadLine();        
+            string country = Console.Write("Your country: ");
+            string country = Console.ReadLine();
+            string comment = Console.Write("Your coment please: ");
+            string comment = Console.ReadLine();
+            Console.WriteLine($"{tittle}" );
+            Console.WriteLine($"{author}" );
+            Console.WriteLine($"{length}" );
+            Console.WriteLine(Video_class.identity);
+            Console.WriteLine(Video_class.house);
+            Console.WriteLine(Video_class.street);
+            Console.WriteLine( Video_class.city  );
+            Console.WriteLine( Video_class.country );
+            Console.WriteLine(Video_class.comment);
+            }
+        }
+    }
+    public class Video_second
+    {
+ 
+    // Static data member of the video class
+        public  string tittle = "Red wolf";
+        public string author ="Johnson moy";
+        public int length = "384";
+        public class Comment_class 
+        {
+ 
+        // Static method of Inner class
+            public static void method2()
+            {
+                string identity = Console.Write("Your personal name ");
+                string identity = Console.ReadLine();
+                string comment = Console.Write("Your coment please: ");
+                string comment = Console.ReadLine();
+        
+                Console.WriteLine(Video_second.tittle);
+                Console.WriteLine(Video_second.author);
+                Console.WriteLine(Video_second.int.parse(Console.ReadLine({length}));
+                Console.WriteLine(Video_second.identity);
+                Console.WriteLine(Video_second.comment);
+            }
+=======
+            string identity = Console.ReadLine();
+            string comment = Console.Write("Your coment please: ");
+            string comment = Console.ReadLine();
+        
+            Console.WriteLine(Video_second.tittle);
+            Console.WriteLine(Video_second.author);
+            Console.WriteLine(Video_second.int.parse(Console.ReadLine({length})));
+            Console.WriteLine(Video_second.identity);
+            Console.WriteLine(Video_second.comment);
+            }
+        }
+    }
+
+    public class Video_third
+    {
+ 
+    // Static data member of the video class
+        public  string tittle = "Blue wolf";
+        public string author ="Joon say";
+        public int length = "484";
+        public class Comment_class 
+        {
+ 
+        // Static method of Inner class
+            public static void method3()
+            {
+                string identity = Console.Write("Your personal name ");
+                string identity = Console.ReadLine();
+                string comment = Console.Write("Your coment please: ");
+                string comment = Console.ReadLine();
+        
+                Console.WriteLine(Video_third.tittle);
+                Console.WriteLine(Video_third.author);
+                Console.WriteLine(Video_third.int.parse(Console.ReadLine({length}));
+                Console.WriteLine(Video_third.identity);
+                Console.WriteLine(Video_third.comment);
+            }
+            string identity = Console.ReadLine();
+            string comment = Console.Write("Your coment please: ");
+            string comment = Console.ReadLine();
+        
+            Console.WriteLine(Video_third.tittle);
+            Console.WriteLine(Video_third.author);
+            Console.WriteLine(Video_third.int.parse(Console.ReadLine({length})));
+            Console.WriteLine(Video_third.identity);
+            Console.WriteLine(Video_third.comment);
+        }
+
+    }
+
+ 
+    public class Video_fourth
+    {
+ 
+    // Static data member of the video class
+        public  string tittle = "Blue wolf";
+        public string author ="Jonson say";
+        public string author ="Joon say";
+        public int length = "484";
+        public class Comment_class 
+        {
+ 
+        // Static method of Inner class
+            public static void method4()
+            {
+                string identity = Console.Write("Your personal name ");
+                string identity = Console.ReadLine();
+                string comment = Console.Write("Your coment please: ");
+                string comment = Console.ReadLine();
+        
+                Console.WriteLine(Video_fourth.tittle);
+                Console.WriteLine(Video_fourth.author);
+                Console.WriteLine(Video_fourth.int.parse(Console.ReadLine({length}));
+                Console.WriteLine(Video_fourth.identity);
+                Console.WriteLine(Video_fourth.comment);
+            }
+        }
+            string identity = Console.ReadLine();
+            string comment = Console.Write("Your coment please: ");
+            string comment = Console.ReadLine();
+        
+            Console.WriteLine(Video_fourth.tittle);
+            Console.WriteLine(Video_fourth.author);
+            Console.WriteLine(Video_fourth.int.parse(Console.ReadLine({length})));
+            Console.WriteLine(Video_fourth.identity);
+            Console.WriteLine(Video_fourth.comment);
+        }
+    
+
+
+// Driver Class
+    public class All_videos 
+    {
+ 
+    // Main method
+        static public void Main()
+        {
+        
+        // Accessing static method1 
+        // of the inner class
+            Video_class.video_class.method1();
+            Video_class.video_second.method2();
+            Video_class.Video_third.method3();
+            Video_class.Video_fourth.method4();
+            }
+        }
+    }       
+}
