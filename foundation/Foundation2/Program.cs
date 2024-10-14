d71370ba52f0ee1c1f9bf709acea9a1c00062b4d
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-14 02:23:25 +0200

    Update Program.cs
    tried a sketch

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,1 +1,56 @@
-fe
+using system;
+class product_label 
+{
+    public virtual void charactOne()
+    {
+       //info here
+        
+        Console.WriteLine("charactOne");
+    }
+}
+
+
+class customer:label
+{
+    public overide void charactOne()
+    {
+
+        // info
+        Console.WriteLine( "    ");
+    }
+}
+
+class address: label
+{
+  public override void charactOne() 
+  {
+
+    //info here
+    Console.WriteLine("       ");
+  }
+}
+
+class order: level
+{
+  public override void charactOne() 
+  {
+    //info here
+    
+    Console.WriteLine("          ");
+  }
+}
+   
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myPig = new Pig();  // Create a Pig object
+    Animal myDog = new Dog();  // Create a Dog object
+
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+} 
