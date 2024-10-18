a3b09b359c00e79684a1d57327105c897640cfc9
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-18 13:54:18 +0200

    Update Program.cs
    added work space and indented

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,26 +1,28 @@
 using System;
-
-class Program
+namespace storage_emulated
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to the mindfulness program!");
-        Console.WriteLine("Describe your self by chosing a number\nrelated to your problem");
-        Console.WriteLine(@"1. Fearful\n2. Stranded\n3.Forgetful ";);
-        string choice = Console.Write("Which number is it?: ");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the mindfulness program!");
+            Console.WriteLine("Describe your self by chosing a number\nrelated to your problem");
+            Console.WriteLine(@"1. Fearful\n2. Stranded\n3.Forgetful ";);
+            string choice = Console.Write("Which number is it?: ");
  
-        int choice = into .parse (Console.ReadLine());
-        Console.WriteLine ($"{choice}");
-        Problem problem = new Problem()
-        problem.Set_stress =  ( "  ");
-        problem.Set_anxious =  ("  ");
+            int choice = into .parse (Console.ReadLine());
+            Console.WriteLine ($"{choice}");
+            Problem problem = new Problem()
+            problem.Set_stress =  ( "  ");
+            problem.Set_anxious =  ("  ");
 
 
-        Solution solution = new Solution()
-        solution.Set_Breath =("  ");
-        solution.Set_Reflect=("  ");
-        solution.Set_List   =("  ");   
+            Solution solution = new Solution()
+            solution.Set_Breath =("  ");
+            solution.Set_Reflect=("  ");
+            solution.Set_List   =("  ");   
 
 
+        }
     }
 }
