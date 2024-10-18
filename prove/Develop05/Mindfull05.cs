ad5006cf95e1d45eda9f07d10a37c313b7b49be7
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-18 09:58:52 +0200

    Update Mindfull05.cs
    added a string of conversations

--- prove/Develop05/Mindfull05.cs
+++ prove/Develop05/Mindfull05.cs
@@ -3,23 +3,23 @@ namespace storage_emulated
 {
     public class Problem
     {
-        protected string _stressed = "1. Fear\n2. Stranded\n3.Forget ";
+        protected string _stressed = "1. Fearful\n2. Stranded\n3.Forgetful ";
         protected string _anxious  = "1. Busy\n2. Forget\n3. Unreliable ";
         public string Get problem()
         {
-            return $"{   }  {  } { } ";
+            return $"Are you{_stressed}?\nAre you{_anxious}?";
         }
         
     }
     
     public class solutionActivity
     {
-        private string _breath = "1. Breath and waitfor10 seconds\2. Breath out and wait for 10 seconds ";
+        private string _breath = "1. Breath & out and waitfor10 seconds\2. Breath out and in & wait for 20 seconds ";
         private string _reflect= "1. Reflect on how your parent suffered\n2. Imitate Jesus\3. Close eyes & imagine God ";
-        private string _list   = "1. List how you feel\2. List what you have learned\n3. List your wishes ";
+        private string _list   = "1. listing how you feel\2. listing what you have learned\n3. listing your wishes ";
         public string GetSolutionActivity()
         {
-            return $"{_breath}{_reflect} {_list}";
+            return $"{_breath}\n and {_reflect}\n {_list}";
            
         }
 
