4f598f993558a6b6d200b5e4d3c50d9519ed5749
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-18 06:30:22 +0200

    Update Mindfull05.cs
    re-sketched

--- prove/Develop05/Mindfull05.cs
+++ prove/Develop05/Mindfull05.cs
@@ -1,2 +1,51 @@
 user system;
 namespace storage_emulated
+{
+    public class Problem
+    {
+        protected string stress
+        {
+
+        }
+        
+        protected string Anxiety
+        {
+
+
+        }
+    
+    public class Solution
+    {
+        protected string Activity
+        {
+            
+
+
+        }
+
+
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
