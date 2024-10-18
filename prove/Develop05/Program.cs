ff91addd39adac3a04e4b556e25c1cb0e3a315d5
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-18 11:34:44 +0200

    Update Program.cs
    hn

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,7 +6,8 @@ class Program
     {
         Console.WriteLine("Welcome to the mindfulness program!");
         Problem problem = new Problem()
-        
+        Problem.Set_stress  =
+        Problem_anxious = 
 
 
         Solution solution = new Solution()
