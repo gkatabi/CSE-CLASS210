3835e590c814dbfe81c5324a277dbe4b5c6225bc
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-20 05:32:44 +0200

    Update Program.cs
    built the console

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -7,6 +7,9 @@ namespace Mindfull
         Problem.SetFear = (" Stressed");
         Problem.SetStranded =( "Stressing");
         Console.WriteLine(Problem.GetFear(Stressed());
-
-    }
+        Activity activity = new Activity ()
+        Activity.SetBreath = ("Deep breath");
+        Activity.SetReflect = ("Archieved");
+        Activity.SetList = ("  ");
+    }.  Console.WriteLine(Activity.GetList( ));
 }
