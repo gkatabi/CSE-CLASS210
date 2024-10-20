22c13171060bf9809f8359e97f6532da07b9eae8
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-20 18:21:10 +0200

    Update Program.cs
    Change it all

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,17 +1,15 @@
 using System;
 namespace Mindfull
 {
-    static void Main(string[] args)
+    public class Problem
     {
-        Problem problem = new Problem()
-        Problem.SetFear = (" Stressed");
-        Problem.SetStranded =( "Stressing");
-        Console.WriteLine(Problem.GetFear(Stressed());
-        Activity activity = new Activity ()
-        Activity.SetBreath = ("Deep breath");
-        Activity.SetReflect = ("Archieved");
-        Activity.SetList = ("  ");
-        Console.WriteLine(Activity.GetList( ));
-
+        static void Main(string[]args)
+        {
+            Problem problem1 = new Problem();
+            Problem1.SetAnxiety = ("forgetting " );
+            Problem2.SetStress =( "bored  ");
+            Console.WriteLine(Problem1.GetProblemInfo());
+        }
     }
 }
+
