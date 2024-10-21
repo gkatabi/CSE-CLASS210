89e3436a1892c5528b51c10dc3d6bc9732d416c4
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-21 22:29:14 +0200

    Update Program.cs
    added braces

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -22,21 +22,25 @@ namespace Mindfull
             Console.ReadKey();
             Thread.sleep(1000);
             if (Chosen =" 1")
+            {
                 Console.WriteLine(Problem1.GetProblemInfo());
                 Console.WriteLine ("Choose from these below");
                 Console.WriteLine("1. I forget things that I plan\2. I am too busy to do it\3. Iam afraid it will take long");
                 Console.Write("Number: ");
                 string number =  (Console.ReadKey());
                 Console.WriteLine($"TThe {number} situation has a solution to it");
-            if int chosen = "2";
+            }
+            if (chosen = "2")
+            {
                 Thread.sleep(1000);
                 Console.WriteLine(Problem2.GetProblemInfo());
                 Comsole.WriteLine("5. Fear\n6. Stranded"); 
-                Console.Writerite("choice is:  ");
-                Console.ReadLine();
-                Console.WriteLine($"The {these} situation has a solution");
+                Console.Writerite("choice is: ");
+                int choice = Convert .ToInt32(Console.ReadLine());
+                Console.WriteLine($"The" + Choice +"th situation has a solution");
                 Console.WriteLine(Solutiob1.GetSolutionInfo());
                 Console.Writeine($"{Solution1.SetActivity} in for 10 {Solution2.SetTime}");
+            }
         }
     }
 }
