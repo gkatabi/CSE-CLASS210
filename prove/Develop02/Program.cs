b178d9298b11258e84efb90a33e7c79ac9a2544e
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-21 13:53:16 +0200

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -10,9 +10,9 @@ MySpace Documents
         Console.ReadKey();
         Console.WriteLine("1. I forgot\n2. I was negligent\n3. No idea\n4. Luck of time\n5. Not sure\n6. Carried away");
         Console.Write("choose a number: ");
-        Console.ReadKey()
-        Number number  = new Number()
-        Comment comment = new Coment()
+        Console.ReadKey();
+        Number number  = new Number();
+        Comment comment = new Coment();
 
 
         Console.WriteLine(" ");
