c331fcec70ee4558cada11ff0154e72f75d3c424
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-21 13:35:06 +0200

    Update Program.cs
    ok

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,8 +8,14 @@ MySpace Documents
         DateTime dateTime = new DateTime();
         Console.WriteLine(dateTime);
         Console.ReadKey();
-        Console.WriteLine("1.I forgot\n2.I was negligent\n3.No idea\n4.Luck of time\n5.Not sure\n6.Carried away");
-        
+        Console.WriteLine("1. I forgot\n2. I was negligent\n3. No idea\n4. Luck of time\n5. Not sure\n6. Carried away");
+        Console.Write("choose a number: ");
+        Console.ReadKey()
+        Number number  = new Number()
+        Comment comment = new Coment()
+
+
+        Console.WriteLine(" ");
 
 
     }
