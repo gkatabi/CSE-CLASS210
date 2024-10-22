c0d045f33c27dd91eb7416d5d6800ac3af5ca048
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 11:20:12 +0200

    Update Program.cs
    line breaking

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //goals sector 
-        Console.WriteLine("It's an eternal quest game application.\We choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures");
+        Console.WriteLine("It's an eternal quest game application.\nWe choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures");
         Console.Write("Which number refers to your goal? ");
         int goal = Convert .ToInt32 (Console.ReadLine());
         Console.WriteLine(goal);
