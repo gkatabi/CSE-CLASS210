07d46cfd530480f6d67e7c09f63d81cd68c92644
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 17:13:47 +0200

    Update Program.cs
    changed the number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,19 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the magic number guess");
-        Console.Write("What is the number ");
-        int guess = int.parse(Console.ReadLine());
+        Console.WriteLine("Welcome to the magic number");
+         number = 5
+        string Number = Console.Write("What is the number? ");
+         
+        string Number= (Console.ReadLine());
         // add if loops here to determinee predictions
         // begin with a while loop to  chain continuity in the game
         
-        int guess = 0;
-        While (guess<14)
+        int number = 5;
+        While (guess<5)
         {
-            if (guess != 13)
-                Console.WriteLine("Guess higher than that");
-            if (guess = 13)
-                Console.WriteLine($"You have guessed right.{guess} is the number");
+            if (guess != 5)
+                Console.WriteLine("Guess is lower than that. guess higher");
+            if (guess = 5)
+                Console.WriteLine($"You have guessed right\n{guess} is the number");
         gues = 13++
         }
     }
