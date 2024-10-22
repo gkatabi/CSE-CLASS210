4a6e2f848076e3cc1bf983f43dcfc274d98d4d86
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 15:11:28 +0200

    Update Program.cs

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -5,13 +5,17 @@ class Program
     static void Main(string[] args)
     {
         //goals sector 
-        Console.WriteLine("It's an eternal quest game application.\nWe choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures");
+        Console.WriteLine("It's an eternal quest game application.\nWe choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures\n5. Quit");
         Console.Write("Which number refers to your goal? ");
         string goals = Console.ReadLine();
-        Console.WriteLine(goals);
-        Console.Write("Which number refers to your goal? ");
         int goal = Convert.ToInt32(Console.ReadLine());
+        goal = 5; 
+        While goal < 5;
+        {
         Console.WriteLine(goal);
+        goal++;
+        }
+
         //Condition user's choices
         //Call for inheritance method
         //if (goal>3;for goal;while
