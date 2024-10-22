6f4d9cfcfe4df069eb1bb2603e643ba94d6d1e77
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 14:25:09 +0200

    Update Develop06
    created it with only empty classes

---
+++ prove/Develop06/Eternalgoals06.cs
@@ -0,0 +1,19 @@
+using system;
+MySpace Documents
+{
+    Public class Goal
+    {
+     
+    }
+    public class Description
+    {
+
+    }
+    Public class Measures
+    {
+    
+    }
+    //Allow creating a new goal
+
+
+}
