d3e93be92a8849d023c52c28d305ae68de8c0295
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 10:59:50 +0200

    Update Program.cs
    numbered goals & prepared for ifs and inheritance

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop06 World!");
+        //goals sector 
+        Console.WriteLine("It's an eternal quest game application.\We choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures");
+        Console.Write("Which number refers to your goal? ");
+        int goal = Convert .ToInt32 (Console.ReadLine());
+        Console.WriteLine(goal);
+        //Condition user's choices
+        //Call for inheritance method
+        //if (goal>3;for goal;while
+        //{
+           Console.Loveme();
+        //} 
+       
+        Intentions intentions = new intentions(oftimes);
+       
+        
     }
 }
