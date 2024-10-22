80db966d5537af56c64e7686146c68d6556e7dfe
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-22 18:23:56 +0200

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,14 +9,24 @@ class Program
         string Number = Console.Write("What is the number? ");
          
         string Number= (Console.ReadLine());
+        int number = Console.ReadLine();
+        if (number== 5)
+        {
+            Console.WriteLine("Wrong guess");
+            do
+            {
+                Console.WriteLine ("Guess again");
+                Console.ReadLine();
+            While (number != 5)
+            }
         // add if loops here to determinee predictions
         // begin with a while loop to  chain continuity in the game
         
-        int number = 5;
+        /*int number = 5;
         While (guess<5)
         {
             if (guess != 5)
-                Console.WriteLine("Guess is lower than that. guess higher");
+                Console.WriteLine("Guess is lower than that. guess higher");*/
             if (guess = 5)
                 Console.WriteLine($"You have guessed right\n{guess} is the number");
         gues = 13++
