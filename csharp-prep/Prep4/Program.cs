4e8b9e33c61c64d19758af3e8200f7694d5b2aa9
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-23 07:58:03 +0200

    Update Program.cs
    indent

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,21 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Write a number");
 
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(number);
+        number = 5;
+        while (number > 0; number < 6)
         {
-            Console.Write("Write a number");
-
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(number);
-            number = 5;
-            while (number > 0; number < 6)
-            {
-                number++
-                Console.ReadLine();
-            }
+            number++
+            Console.ReadLine();
             List<int> duplets;
             duplets = new List<int>();
             duplets.Add("number");
+            }
+           // List<int> duplets;
+            //duplets = new List<int>();
+            //duplets.Add("number");
             
 
         }
