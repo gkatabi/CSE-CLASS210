9425ee26c316f511bfa7e5b3b275fca8fd909308
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-23 09:49:00 +0200

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,30 @@
-/*using System;
+using System;
+namespace Documents
+{
+    public class Invitation(welcomes)
+    {
+        return //Console.WriteLine("Welcome to the program!");
+    } 
 
-class Cars
-{ 
+    public class Initials(about)
+    {
+       _first name;
+       _last name;
+    }
+
+    public class 
+
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Invitation invitation = new Invitation ();
+        string invitation =("Welcome to the program!"
+     
+
+        Initials initials = new Initials();
+        
+
+
     }
 }*/
