6f9c4a452fe0399398a4527251fc26a87d76ded7
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-23 04:40:48 +0200

    Update Program.cs
    created a list baquet

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,11 +1,17 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //goals sector
         List<string>goals;
         goals = new List<string>();
+        goals.Add("1. Care or own a family");
+        goals.Add("2. Serve church callings");
+        goals.Add("3. Work to earn");
+        goals.Add("4. Study scriptures ");
+        goals.Add("5. Quit");
         //goals sector 
         Console.WriteLine("It's an eternal quest game application.\nWe choose and pursue any of the following goals:\n1. Care or own a family\n2. Serve church callings\n3. Work to earn\n4. Study scriptures\n5. Quit");
         Console.Write("Which number refers to your goal? ");
