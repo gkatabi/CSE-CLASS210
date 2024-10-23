e14a66e925d552f68f4118b375731f2520522729
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-23 07:50:34 +0200

    Update Program.cs hg

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,21 +1,29 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        number = 0;
-        while (number > 0)
-        {
-            Console.WriteLine("Write a number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.ReadLine();
-            List<int>duplets;
-            duplets = new List<int>();
-            {
-            duplets.Add ("number");
-            }
 
-        }  
+        {
+            Console.Write("Write a number");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+            number = 5;
+            while (number > 0; number < 6)
+            {
+                number++
+                Console.ReadLine();
+            }
+            List<int> duplets;
+            duplets = new List<int>();
+            duplets.Add("number");
+            
+
+        }
     }
 }
+
+
+
+
