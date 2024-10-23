7a8ba807ad70172630e5ece5cd2d49eebf820f5b
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2024-10-23 05:46:05 +0200

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        number = 0;
+        while (number > 0)
+        {
+            Console.WriteLine("Write a number");
+            Console.ReadLine();
+            List<int>Numbers;
+            numbers = new List<into>();
+            foreach (number in numbers)
+            {
+            answer.Add "number"
+            }
+
+        }  
     }
 }
